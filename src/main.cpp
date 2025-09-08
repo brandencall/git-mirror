4ce5611e8fcb31662eeb67c624454dd3a5ab9a84
Branden Call <brandencall@live.com>
2025-09-08 17:11:28 -0600

    Git clone and git pull logic in

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include "utils_git/Repo.h"
 #include "utils_git/github_request.h"
 #include "utils_git/load_config.h"
+#include <filesystem>
+#include <iostream>
+#include <string>
 #include <vector>
 
 int main() {
+    const char *home_env = std::getenv("HOME");
+    std::filesystem::path homePath = home_env;
+    std::filesystem::path projectsDir = homePath / "projects";
+    std::filesystem::create_directory(projectsDir);
     std::vector<std::string> excludeRepos = utils_git::loadRepoConfig("exclude_repos.conf");
     std::vector<utils_git::Repo> repos = utils_git::getRepos(excludeRepos);
     for (const auto &repo : repos) {
-        repo.printRepo();
+        std::filesystem::path repoPath = projectsDir / repo._name;
+        if (!std::filesystem::is_directory(repoPath)) {
+                std::string cmd = "cd " + projectsDir.string() + " && git clone " + repo._sshURL;
+                std::cout << "Cloning: " << cmd << '\n';
+                std::system(cmd.c_str());
+        } else {
+            std::string cmd = "cd " + repoPath.string() + " && git pull";
+            std::cout << "Pulling: " << cmd << '\n';
+            std::system(cmd.c_str());
+        }
     }
-
     return 0;
 }
